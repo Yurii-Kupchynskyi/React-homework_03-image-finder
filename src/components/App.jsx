b00f49331a358a4245e7d95387b7660608e4423b
Yurii <yurakupchunsky@gmail.com>
2024-04-15 00:38:51 +0300

    Starting template

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,31 +1,11 @@
 import { Component } from 'react';
 import { Box } from './Box';
-import Video from './VideoPlayer/Video';
-import videosData from './videos.json';
-import Player from './VideoPlayer/Player';
-import Statistic from './Feedback/Statistic';
-import Phonebook from './Phonebook/Phonebook';
 
 export class App extends Component {
-  state = {
-    selectedVideo: null,
-  };
-  selectVideo = link => {
-    this.setState({ selectedVideo: link });
-  };
   render() {
-    const { selectedVideo } = this.state;
-    // console.table(videos);
     return (
       <Box as="main">
-        <Video
-          items={videosData}
-          onSelect={this.selectVideo}
-          selected={selectedVideo}
-        />
-        {selectedVideo && <Player link={selectedVideo} />}
-        <Statistic />
-        <Phonebook />
+        <h1>Start template</h1>
       </Box>
     );
   }
